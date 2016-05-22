0dd6b89fe5ba5af1242675f07b2562478dcfbd99
Azura <darkazuredragon@mail.ru>
2016-05-22 19:06:15 +0300

    Changed to a a dinamic array to work with functions. + testing map

--- main.cpp
+++ main.cpp
@@ -2,36 +2,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <map>
 
 using namespace std;
 
+
+int findDifferentArray(int *A, int N);
+//int A - массив А
+//N - число элементов в массиве А
+
+
+int findDifferentArray(int *A, int N)
+{
+    map <int,int> unicNum;
+    unicNum[1]=2;
+    unicNum[3]=5;
+    unicNum[6]=6;
+
+ //массив размером N для уникальных элементов
+    int i,j,R,P; //счётчик массива; кол-во уникальных элементов; индекс последнего уникального элемента
+    P=0;
+
+
+ cout << "unicNum has: " << endl;
+
+ for (map<int,int>::iterator it = unicNum.begin(); it != unicNum.end(); ++it)
+ {
+     cout << it->first << " ... " << it->second << endl;
+ }
+
+ return P;
+}
+
+
+
+
 int main()
 {
- int i,N;
+    int i,N;
+    cout<<"Enter length of array: "<<endl;
+    cin>>N;
+    cout<<"N="<<N<<endl;
 
- cout<<"Enter length of array: "<<endl;
- cin>>N;
- cout<<"N="<<N<<endl;
+    int *A = new int [N];
 
- int A[N];
- srand (time(NULL));
+    srand (time(NULL));
 
- for(i=0;i<N;i++)
- {
-    A[i]=rand() % 200 - 100;
- }
- cout<<"An array was generated! "<<endl;
- cout<<"[";
- for(i=0;i<N;i++)
- {
-    if (i==N-1)
+    for(i=0;i<N;i++)
     {
-        cout<<A[i];
-        break;
+        A[i]=rand() % 200 - 100;
     }
-    cout<<A[i]<<", ";
- }
- cout<<"]"<<endl;
 
- return 0;
+    cout<<"An array was generated! "<<endl;
+
+    cout<<"[";
+
+    for(i=0;i<N;i++)
+    {
+        if (i==N-1)
+        {
+            cout<<A[i];
+            break;
+        }
+        cout<<A[i]<<", ";
+    }
+
+    cout<<"]"<<endl;
+    findDifferentArray(A,N);
+
+    cout <<"cleaning after ourselves"<<endl;
+    cout <<"Deleting array A"<<endl;
+    delete [] A;
+    cout <<"Done!"<<endl;
+
+    return 0;
 }
+
+
